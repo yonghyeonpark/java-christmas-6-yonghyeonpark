1f529831550e168e6715789e8dbc2a6dd0395e08
YongHyeon <pyh4616@naver.com>
2023-11-10 02:00:50 +0900

    feat(Calendar): 날짜가 평일인지 주말인지 판단하는 기능 구현

---
+++ src/main/java/christmas/model/Calendar.java
@@ -0,0 +1,28 @@
+package christmas.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendar {
+
+    private final int date;
+    private List<Integer> weekend;
+    private List<Integer> specialDay;
+
+    public Calendar(int date) {
+        this.date = date;
+        initialize();
+    }
+
+    private void initialize() {
+        weekend = List.of(1,2,8,9,15,16,22,23,29,30);
+        specialDay = List.of(3,10,17,24,25,31);
+    }
+
+    public String judgeIsWeekdayOrWeekend(int date) {
+        if (weekend.contains(date)) {
+            return "weekend";
+        }
+        return "weekday";
+    }
+}
