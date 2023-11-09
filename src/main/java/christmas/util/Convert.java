a602c6fbb7151b504c861549fb55f5494bcc37a9
YongHyeon <pyh4616@naver.com>
2023-11-10 02:32:09 +0900

    feat(Convert): 문자열을 리스트로 변환하는 기능 구현

--- src/main/java/christmas/util/Convert.java
+++ src/main/java/christmas/util/Convert.java
@@ -1,5 +1,9 @@
 package christmas.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Convert {
 
     private static final String NON_INTEGER_INPUT_ERROR_MESSAGE = "[ERROR] 공백이나 숫자가 아닌 입력은 허용하지 않습니다. 다시 입력해 주세요.";
@@ -10,5 +14,10 @@ public class Convert {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NON_INTEGER_INPUT_ERROR_MESSAGE);
         }
+    }
+
+    public static List<String> StringToList(String input) {
+        return Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
     }
 }
