619f9a9552fd716677cfdf643eff19e9a08bc8d2
YongHyeon <pyh4616@naver.com>
2023-11-10 01:27:45 +0900

    feat(Convert): 문자열을 정수로 변환하는 과정에서 NumberFormatException이 발생하면 그것을 잡아 IllegalArgumentException을 고의로 발생시키는 기능 구현

--- src/main/java/christmas/util/Convert.java
+++ src/main/java/christmas/util/Convert.java
@@ -2,7 +2,13 @@ package christmas.util;
 
 public class Convert {
 
+    private static final String NON_INTEGER_INPUT_ERROR_MESSAGE = "[ERROR] 공백이나 숫자가 아닌 입력은 허용하지 않습니다. 다시 입력해 주세요.";
+
     public static int StringToInteger(String input) {
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NON_INTEGER_INPUT_ERROR_MESSAGE);
+        }
     }
 }
