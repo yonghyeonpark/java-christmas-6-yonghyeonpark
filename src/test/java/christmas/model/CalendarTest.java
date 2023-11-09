fd3a6349ab0a5b7e8c177569ad4f97be6a65e0cd
YongHyeon <pyh4616@naver.com>
2023-11-10 02:24:28 +0900

    test(CalendarTest): 날짜가 특별한 날인지 판단하는 기능을 테스트

--- src/test/java/christmas/model/CalendarTest.java
+++ src/test/java/christmas/model/CalendarTest.java
@@ -3,6 +3,7 @@ package christmas.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,5 +16,14 @@ public class CalendarTest {
         Calendar calendar = new Calendar(date);
 
         assertThat(calendar.judgeIsWeekdayOrWeekend()).isEqualTo(day);
+    }
+
+    @DisplayName("날짜가 특별한 날인지 판단한다.")
+    @Test
+    void judgeIsSpecialDay() {
+        int date = 10;
+        Calendar calendar = new Calendar(date);
+
+        assertThat(calendar.judgeIsSpecialDay()).isEqualTo("specialDay");
     }
 }
