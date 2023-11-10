a4dcb7b7e69f7e04a3dd2dc549e5a6caa3cc19af
YongHyeon <pyh4616@naver.com>
2023-11-10 15:43:49 +0900

    refactor(Convert): 잘못된 메서드명 양식(첫 글자가 대문자) 수정

--- src/main/java/christmas/util/Convert.java
+++ src/main/java/christmas/util/Convert.java
@@ -10,7 +10,7 @@ public class Convert {
 
     private static final String NON_INTEGER_INPUT_ERROR_MESSAGE = "[ERROR] 공백이나 숫자가 아닌 입력은 허용하지 않습니다. 다시 입력해 주세요.";
 
-    public static int StringToInteger(String input) {
+    public static int stringToInteger(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -18,7 +18,7 @@ public class Convert {
         }
     }
 
-    public static List<String> StringToList(String input) {
+    public static List<String> stringToList(String input) {
         return Arrays.stream(input.split(","))
                 .collect(Collectors.toList());
     }
@@ -28,7 +28,7 @@ public class Convert {
         for (String nonSplitOrder : nonSplitOrders) {
             List<String> splitOrder = Arrays.stream(nonSplitOrder.split("-"))
                     .toList();
-            orders.put(splitOrder.get(0), Convert.StringToInteger(splitOrder.get(1)));
+            orders.put(splitOrder.get(0), Convert.stringToInteger(splitOrder.get(1)));
         }
         return orders;
     }
