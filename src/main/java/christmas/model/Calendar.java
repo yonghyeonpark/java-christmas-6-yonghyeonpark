ec8d16f6b2139ee84279dd3c194e1a841cc70fe0
YongHyeon <pyh4616@naver.com>
2023-11-11 04:03:54 +0900

    feat(Calendar): 입력 날짜의 범위에 대한 유효성 검사 기능 구현

--- src/main/java/christmas/model/Calendar.java
+++ src/main/java/christmas/model/Calendar.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public class Calendar {
 
+    private static final String DATE_INPUT_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
     private final int date;
     private List<Integer> weekend;
     private List<Integer> specialDay;
 
     public Calendar(int date) {
         this.date = date;
+        validateRange(date);
         initialize();
+    }
+
+    private void validateRange(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException(DATE_INPUT_ERROR_MESSAGE);
+        }
     }
 
     private void initialize() {
