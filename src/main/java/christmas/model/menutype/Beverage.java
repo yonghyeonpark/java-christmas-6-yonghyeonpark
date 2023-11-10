708567a0a04d4baf48c61eae330ecae5f0e40822
YongHyeon <pyh4616@naver.com>
2023-11-11 00:43:20 +0900

    feat(Beverage): 메뉴 타입 중 음료를 관리하는 enum 구현

---
+++ src/main/java/christmas/model/menutype/Beverage.java
@@ -0,0 +1,16 @@
+package christmas.model.menutype;
+
+public enum Beverage {
+
+    ZERO_COLA("제로콜라", 3_000),
+    RED_WINE("레드와인", 60_000),
+    CHAMPAGNE("샴페인", 25_000);
+
+    private String name;
+    private int price;
+
+    Beverage(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
