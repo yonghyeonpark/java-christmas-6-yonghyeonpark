3d8edfce6a9d06c18c87dfea28c1f8f794d05466
YongHyeon <pyh4616@naver.com>
2023-11-11 03:45:04 +0900

    test(MenuTypeTest): 주문에 대해 메뉴 종류를 분류하고 각 개수를 구하는 기능 테스트

---
+++ src/test/java/christmas/model/MenuTypeTest.java
@@ -0,0 +1,30 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MenuTypeTest {
+
+    @DisplayName("주문에 대해 메뉴 종류를 분류하고 각 개수를 구한다.")
+    @Test
+    void countMenuType() {
+        Map<String, Integer> menuType = new HashMap<>();
+        MenuType menu = new MenuType(menuType);
+        menu.countMenuType(Map.of(
+                "양송이수프", 1,
+                "타파스", 2,
+                "티본스테이크", 3,
+                "아이스크림", 4,
+                "레드와인", 5
+        ));
+
+        assertThat(menuType.get("애피타이저")).isEqualTo(3);
+        assertThat(menuType.get("메인")).isEqualTo(3);
+        assertThat(menuType.get("디저트")).isEqualTo(4);
+        assertThat(menuType.get("음료")).isEqualTo(5);
+    }
+}
