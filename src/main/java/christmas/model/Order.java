e33ef431c1dac292ac5e607a6792a385faf691ad
YongHyeon <pyh4616@naver.com>
2023-11-11 04:54:01 +0900

    feat(Order): 주문 메뉴 중복 여부에 대한 유효성 검사 기능 구현

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,12 +1,24 @@
 package christmas.model;
 
+import christmas.util.Convert;
+import java.util.List;
 import java.util.Map;
 
 public class Order {
 
+    private static final String ORDER_INPUT_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
     private final Map<String, Integer> orders;
 
-    public Order(Map<String, Integer> orders) {
-        this.orders = orders;
+    public Order(List<String> orders) {
+        Map<String, Integer> mappedOrders = Convert.listToMapWithSplit(orders);
+        validateDuplicate(orders, mappedOrders);
+        this.orders = mappedOrders;
+    }
+
+    private void validateDuplicate(List<String> orders, Map<String, Integer> mappedOrders) {
+        if (orders.size() != mappedOrders.size()) {
+            throw new IllegalArgumentException(ORDER_INPUT_ERROR_MESSAGE);
+        }
     }
 }
