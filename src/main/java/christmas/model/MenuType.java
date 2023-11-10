94d46c8742a9ab16f8dc175a02bd2a819a381275
YongHyeon <pyh4616@naver.com>
2023-11-11 03:40:15 +0900

    feat(MenuType): 주문에 대해 메뉴 종류를 분류하고 각 개수를 구하는 기능 구현

---
+++ src/main/java/christmas/model/MenuType.java
@@ -0,0 +1,63 @@
+package christmas.model;
+
+import christmas.model.menu.Appetizer;
+import christmas.model.menu.Beverage;
+import christmas.model.menu.Dessert;
+import christmas.model.menu.Main;
+import java.util.Map;
+
+public class MenuType {
+
+    private final Map<String, Integer> menuType;
+
+    public MenuType(Map<String, Integer> menuType) {
+        this.menuType = menuType;
+        setUp();
+    }
+
+    private void setUp() {
+        menuType.put("애피타이저", 0);
+        menuType.put("메인", 0);
+        menuType.put("디저트", 0);
+        menuType.put("음료", 0);
+    }
+
+    public void countMenuType(Map<String, Integer> orders) {
+        countAppetizer(orders);
+        countMain(orders);
+        countDessert(orders);
+        countBeverage(orders);
+    }
+
+    private void countAppetizer(Map<String, Integer> orders) {
+        for (Appetizer appetizer : Appetizer.values()) {
+            if (orders.containsKey(appetizer.getName())) {
+                menuType.put("애피타이저", menuType.get("애피타이저") + orders.get(appetizer.getName()));
+            }
+        }
+    }
+
+    private void countMain(Map<String, Integer> orders) {
+        for (Main main : Main.values()) {
+            if (orders.containsKey(main.getName())) {
+                menuType.put("메인", menuType.get("메인") + orders.get(main.getName()));
+            }
+        }
+    }
+
+    private void countDessert(Map<String, Integer> orders) {
+        for (Dessert dessert : Dessert.values()) {
+            if (orders.containsKey(dessert.getName())) {
+                menuType.put("디저트", menuType.get("디저트") + orders.get(dessert.getName()));
+            }
+        }
+    }
+
+    private void countBeverage(Map<String, Integer> orders) {
+        for (Beverage beverage : Beverage.values()) {
+            if (orders.containsKey(beverage.getName())) {
+                menuType.put("음료", menuType.get("음료") + orders.get(beverage.getName()));
+            }
+        }
+    }
+}
