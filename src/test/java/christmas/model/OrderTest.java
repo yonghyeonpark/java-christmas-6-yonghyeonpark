5391da4b4b756016c6d3020c62ec7e3137e0acda
YongHyeon <pyh4616@naver.com>
2023-11-11 04:55:43 +0900

    test(OrderTest): 주문 메뉴 중복 여부에 대한 유효성 검사 기능 테스트

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -1,4 +1,18 @@
 package christmas.model;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class OrderTest {
+
+    @DisplayName("주문 메뉴에 대해 중복이 있으면 예외가 발생한다.")
+    @Test
+    void should_ThrowException_When_MenuDuplicate() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Order(List.of("티본스테이크-1", "티본스테이크-2", "양송이수프-1")))
+                .withMessageContaining("[ERROR]");
+    }
 }
