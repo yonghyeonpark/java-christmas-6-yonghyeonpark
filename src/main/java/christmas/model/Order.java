05a8ef7c31de7895ae91b7f1d125bfdd7823b2b9
YongHyeon <pyh4616@naver.com>
2023-11-11 04:58:43 +0900

    feat(Order): 주문 메뉴의 총개수에 대한 유효성 검사 기능 구현

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -13,11 +13,22 @@ public class Order {
     public Order(List<String> orders) {
         Map<String, Integer> mappedOrders = Convert.listToMapWithSplit(orders);
         validateDuplicate(orders, mappedOrders);
+        validateRange(mappedOrders);
         this.orders = mappedOrders;
     }
 
     private void validateDuplicate(List<String> orders, Map<String, Integer> mappedOrders) {
         if (orders.size() != mappedOrders.size()) {
+            throw new IllegalArgumentException(ORDER_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateRange(Map<String, Integer> orders) {
+        int orderCount = 0;
+        for (Map.Entry<String, Integer> entry : orders.entrySet()) {
+            orderCount += entry.getValue();
+        }
+        if (orderCount > 20) {
             throw new IllegalArgumentException(ORDER_INPUT_ERROR_MESSAGE);
         }
     }
