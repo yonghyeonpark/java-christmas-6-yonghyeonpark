5148d83d8990fc21afadb80599972a8e6cf7ab9e
YongHyeon <pyh4616@naver.com>
2023-11-12 20:40:47 +0900

    refactor(ConvertTest): 메서드 인자 변경사항을 테스트에도 적용

--- src/test/java/christmas/util/ConvertTest.java
+++ src/test/java/christmas/util/ConvertTest.java
@@ -3,6 +3,7 @@ package christmas.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import christmas.model.ErrorMessage;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -17,8 +18,7 @@ public class ConvertTest {
     @ValueSource(strings = {"", "a"})
     void should_ThrowIllegalArgumentException_When_InputBlankOrNonInteger(String input) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Convert.stringToInteger(input))
-                .withMessageContaining("[ERROR]");
+                .isThrownBy(() -> Convert.stringToInteger(input, "[ERROR] content"));
     }
 
     @DisplayName("문자열을 쉼표 기준으로 분리하여 리스트에 저장한다.")
@@ -35,7 +35,10 @@ public class ConvertTest {
     @DisplayName("주문리스트를 각각 메뉴와 수량으로 분리하여 Map에 저장한다.")
     @Test
     void listToMapWithSplit() {
-        Map<String, Integer> orders = Convert.listToMapWithSplit(List.of("시저샐러드-1", "티본스테이크-2"));
+        Map<String, Integer> orders = Convert.listToMapWithSplit(
+                List.of("시저샐러드-1", "티본스테이크-2"),
+                ErrorMessage.ORDER.getContent()
+        );
 
         assertThat(orders.get("시저샐러드")).isEqualTo(1);
         assertThat(orders.get("티본스테이크")).isEqualTo(2);
