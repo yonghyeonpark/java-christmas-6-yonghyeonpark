a0ca3526d6de0175a4d2c2c04f1589c58dd99d6e
YongHyeon <pyh4616@naver.com>
2023-11-13 02:51:05 +0900

    refactor(Order): 에러 메시지 상수를 enum에서 받아오도록 수정

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class Order {
 
-    private static final String ORDER_INPUT_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String ORDER_INPUT_ERROR_MESSAGE = ErrorMessage.ORDER.getContent();
 
     private final Map<String, Integer> orders;
 
