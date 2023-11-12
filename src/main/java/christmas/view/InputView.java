657b4dc7d1f813b446662b490f30f7cb09f39e82
YongHyeon <pyh4616@naver.com>
2023-11-12 18:53:08 +0900

    refactor(InputView): 예외 처리를 Convert가 아닌 다른 클래스에서 진행하기 위해 입력 메서드의 반환 타입을 String으로 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,23 +1,19 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.util.Convert;
-import java.util.List;
 
 public class InputView {
 
     private static final String VISIT_DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_INPUT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public int readDate() {
+    public String readDate() {
         System.out.println(VISIT_DATE_INPUT_MESSAGE);
-        String input = Console.readLine();
-        return Convert.stringToInteger(input);
+        return Console.readLine();
     }
 
-    public List<String> readOrder() {
+    public String readOrder() {
         System.out.println(ORDER_INPUT_MESSAGE);
-        String input = Console.readLine();
-        return Convert.stringToList(input);
+        return Console.readLine();
     }
 }
