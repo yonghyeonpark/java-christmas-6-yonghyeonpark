e5d88762e58bd1357a7e008aefcaec509cd663b5
YongHyeon <pyh4616@naver.com>
2023-11-13 21:45:02 +0900

    refactor(Benefit): 메서드에 사용하기 위해 Calendar 타입의 인스턴스 변수 추가 및 메서드 수정

--- src/main/java/christmas/model/Benefit.java
+++ src/main/java/christmas/model/Benefit.java
@@ -5,15 +5,16 @@ import java.util.Map;
 public class Benefit {
 
     private static final int INITIAL_AMOUNT = 0;
-    private static final int CHRISTMAS_DAY = 25;
+    private static final int NOTHING = -1;
     private static final int D_DAY_DISCOUNT_BASIC_AMOUNT = 1000;
     private static final int D_DAY_DISCOUNT_INCREMENT_AMOUNT = 100;
-    private static final int ONE = 1;
 
     private final Map<BenefitType, Integer> benefits;
+    private final Calendar calendar;
 
-    public Benefit(Map<BenefitType, Integer> benefits) {
+    public Benefit(Map<BenefitType, Integer> benefits, Calendar calendar) {
         this.benefits = benefits;
+        this.calendar = calendar;
         setUp();
     }
 
@@ -25,12 +26,14 @@ public class Benefit {
         benefits.put(BenefitType.GIFT_EVENT, INITIAL_AMOUNT);
     }
 
-    public void calculateDDayDiscount(int date) {
-        if (date <= CHRISTMAS_DAY) {
+    public void calculateDDayDiscountAmount() {
+        int calculation = calendar.CalculationForDDayDiscountAmount();
+        if (calculation != NOTHING) {
             benefits.put(
                     BenefitType.CHRISTMAS_D_DAY,
-                    D_DAY_DISCOUNT_BASIC_AMOUNT + ((date - ONE) * D_DAY_DISCOUNT_INCREMENT_AMOUNT)
+                    D_DAY_DISCOUNT_BASIC_AMOUNT + (calculation * D_DAY_DISCOUNT_INCREMENT_AMOUNT)
             );
         }
     }
+
 }
