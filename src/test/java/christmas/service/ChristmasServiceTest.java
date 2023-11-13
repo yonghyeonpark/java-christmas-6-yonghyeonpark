2ec08d83fe800e158731db461abf73b1c35c87ce
YongHyeon <pyh4616@naver.com>
2023-11-14 04:48:25 +0900

    test(ChristmasServiceTest): 할인 후 예상 결제 금액을 계산하는 기능 테스트

--- src/test/java/christmas/service/ChristmasServiceTest.java
+++ src/test/java/christmas/service/ChristmasServiceTest.java
@@ -3,6 +3,7 @@ package christmas.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -28,5 +29,17 @@ public class ChristmasServiceTest {
         String giftMenu = christmasService.getGiftMenu(totalOrderAmount);
 
         assertThat(giftMenu).isEqualTo(expected);
+    }
+
+    @DisplayName("할인 후 예상 결제 금액을 계산한다.")
+    @Test
+    void calculatePaymentAmount() {
+        ChristmasService christmasService = new ChristmasService();
+        int totalOrderAmount = 30000;
+        int totalDiscountAmount = 10000;
+
+        int paymentAmount = christmasService.calculatePaymentAmount(totalOrderAmount, totalDiscountAmount);
+
+        assertThat(paymentAmount).isEqualTo(20000);
     }
 }
