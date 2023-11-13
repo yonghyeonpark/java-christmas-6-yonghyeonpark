3fc9d074cf45055746bc8f6450265c29ece0773e
YongHyeon <pyh4616@naver.com>
2023-11-14 03:41:11 +0900

    feat(ChristmasService): 할인 전 총주문 금액에 따라 증정 메뉴를 구하는 기능 구현

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,11 +1,18 @@
 package christmas.service;
 
 public class ChristmasService {
-    
+
     public boolean isEventApplication(int totalOrderAmount) {
         if (totalOrderAmount >= 10000) {
             return true;
         }
         return false;
+    }
+
+    public String getGiftMenu(int totalOrderAmount) {
+        if (totalOrderAmount >= 120000) {
+            return "샴페인 1개";
+        }
+        return "없음";
     }
 }
