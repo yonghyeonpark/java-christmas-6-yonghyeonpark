9774c7692ff5a30d42a4beddc895585ff741a9c5
YongHyeon <pyh4616@naver.com>
2023-11-13 23:08:50 +0900

    test(BenefitTest): 평일 할인 금액 혹은 주말 할인 금액을 계산하는 기능 테스트

--- src/test/java/christmas/model/BenefitTest.java
+++ src/test/java/christmas/model/BenefitTest.java
@@ -1,9 +1,11 @@
 package christmas.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,6 +20,36 @@ public class BenefitTest {
 
         benefit.calculateDDayDiscountAmount();
 
-        Assertions.assertThat(benefits.get(BenefitType.CHRISTMAS_D_DAY)).isEqualTo(expected);
+        assertThat(benefits.get(BenefitType.CHRISTMAS_D_DAY)).isEqualTo(expected);
+    }
+
+    @DisplayName("평일 할인 금액을 계산한다.")
+    @Test
+    void calculateWeekDayDiscountAmount() {
+        Map<BenefitType, Integer> benefits = new HashMap<>();
+        Benefit benefit = new Benefit(benefits, new Calendar("17"));
+        Map<String, Integer> menuType = new HashMap<>();
+        menuType.put("메인", 2);
+        menuType.put("디저트", 3);
+
+        benefit.calculateWeekDayOrWeekendDiscountAmount(menuType);
+
+        assertThat(benefits.get(BenefitType.WEEKDAY)).isEqualTo(6069);
+        assertThat(benefits.get(BenefitType.WEEKEND)).isEqualTo(0);
+    }
+
+    @DisplayName("주말 할인 금액을 계산한다.")
+    @Test
+    void calculateWeekendDiscountAmount() {
+        Map<BenefitType, Integer> benefits = new HashMap<>();
+        Benefit benefit = new Benefit(benefits, new Calendar("15"));
+        Map<String, Integer> menuType = new HashMap<>();
+        menuType.put("메인", 2);
+        menuType.put("디저트", 3);
+
+        benefit.calculateWeekDayOrWeekendDiscountAmount(menuType);
+
+        assertThat(benefits.get(BenefitType.WEEKDAY)).isEqualTo(0);
+        assertThat(benefits.get(BenefitType.WEEKEND)).isEqualTo(4046);
     }
 }
