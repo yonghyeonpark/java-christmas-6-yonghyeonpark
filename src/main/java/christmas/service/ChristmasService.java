d520bf306bfb1d55e953c49ba5b09af4bcd3b303
YongHyeon <pyh4616@naver.com>
2023-11-14 05:12:25 +0900

    feat(ChristmasService): 총혜택 금액에 따라 이벤트 배지 종류를 판단하는 기능 구현

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -19,4 +19,29 @@ public class ChristmasService {
     public int calculatePaymentAmount(int totalOrderAmount, int totalDiscountAmount) {
         return totalOrderAmount - totalDiscountAmount;
     }
+
+    public String judgeEventBadge(int totalBenefitAmount) {
+        if (isStarBadge(totalBenefitAmount)) {
+            return "별";
+        }
+        if (isTreeBadge(totalBenefitAmount)) {
+            return "트리";
+        }
+        if (isSantaBadge(totalBenefitAmount)) {
+            return "산타";
+        }
+        return "없음";
+    }
+
+    private boolean isStarBadge(int totalBenefitAmount) {
+        return (totalBenefitAmount >= 5000) && (totalBenefitAmount < 10000);
+    }
+
+    private boolean isTreeBadge(int totalBenefitAmount) {
+        return (totalBenefitAmount >= 10000) && (totalBenefitAmount < 20000);
+    }
+
+    private boolean isSantaBadge(int totalBenefitAmount) {
+        return totalBenefitAmount >= 20000;
+    }
 }
