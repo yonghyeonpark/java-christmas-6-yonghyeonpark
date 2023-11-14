193dcaa948a4a6f15b0ad642353864edccc9f825
YongHyeon <pyh4616@naver.com>
2023-11-14 13:44:38 +0900

    feat(OutputView): 진행 시작 메시지를 출력하는 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+
+    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+}
