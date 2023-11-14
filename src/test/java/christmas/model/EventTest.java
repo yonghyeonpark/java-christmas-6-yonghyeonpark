1f9e25515c79135c1364203c6cc34b6239d48b24
YongHyeon <pyh4616@naver.com>
2023-11-15 05:55:22 +0900

    refactor(EventTest): 공통된 객체 생성 행위의 중복 제거

--- src/test/java/christmas/model/EventTest.java
+++ src/test/java/christmas/model/EventTest.java
@@ -2,6 +2,7 @@ package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,12 +10,17 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class EventTest {
 
+    private Event event;
+
+    @BeforeEach
+    void setUp() {
+        event = new Event();
+    }
+
     @DisplayName("할인 전 총주문 금액에 따라 이벤트 적용 여부를 판단한다.")
     @ParameterizedTest(name = "[테스트 케이스 {index}] 총주문 금액 : {0}, 이벤트 적용 여부 : {1}")
     @CsvSource({"9999, false", "10000, true"})
     void isEventApplication(int totalOrderAmount, boolean expected) {
-        Event event = new Event();
-
         boolean eventApplication = event.isEventApplication(totalOrderAmount);
 
         assertThat(eventApplication).isEqualTo(expected);
@@ -24,8 +30,6 @@ public class EventTest {
     @ParameterizedTest(name = "[테스트 케이스 {index}] 총주문 금액 : {0}, 증정 메뉴 : {1}")
     @CsvSource({"119999, 없음", "120000, 샴페인 1개"})
     void getGiftMenu(int totalOrderAmount, String expected) {
-        Event event = new Event();
-
         String giftMenu = event.getGiftMenu(totalOrderAmount);
 
         assertThat(giftMenu).isEqualTo(expected);
@@ -34,7 +38,6 @@ public class EventTest {
     @DisplayName("할인 후 예상 결제 금액을 계산한다.")
     @Test
     void calculatePaymentAmount() {
-        Event event = new Event();
         int totalOrderAmount = 30000;
         int totalDiscountAmount = 10000;
 
@@ -47,8 +50,6 @@ public class EventTest {
     @ParameterizedTest(name = "[테스트 케이스 {index}] 총혜택 금액 : {0}, 이벤트 뱃지 : {1}")
     @CsvSource({"4999, 없음", "5000, 별", "9999, 별", "10000, 트리", "19999, 트리", "20000, 산타"})
     void getEventBadge(int totalBenefitAmount, String expected) {
-        Event event = new Event();
-
         String eventBadge = event.judgeEventBadge(totalBenefitAmount);
 
         assertThat(eventBadge).isEqualTo(expected);
