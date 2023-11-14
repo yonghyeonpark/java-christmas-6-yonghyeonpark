c7eeaf2fe5094b51e2513599f7385109db5c770d
YongHyeon <pyh4616@naver.com>
2023-11-15 02:28:56 +0900

    feat(Application): 프로그램 실행 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ChristmasController christmasController = new ChristmasController(inputView, outputView);
+        christmasController.createEventPlanner();
     }
 }
