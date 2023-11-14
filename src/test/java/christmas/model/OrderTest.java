d0e744bf9ea80a1b3cfd49d7886a3848d90776fa
YongHyeon <pyh4616@naver.com>
2023-11-14 20:04:28 +0900

    refactor(OrderTest): 클래스(Order)의 생성자 인자 변경 반영

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -3,18 +3,27 @@ package christmas.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.HashMap;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
 
+    private MenuType menuType;
+
+    @BeforeEach
+    void setUp() {
+        menuType = new MenuType(new HashMap<>());
+    }
+
     @DisplayName("주문 메뉴에 대해 중복이 있으면 예외가 발생한다.")
     @Test
     void should_ThrowException_When_MenuDuplicate() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Order(List.of("티본스테이크-1", "티본스테이크-2", "양송이수프-1")))
+                .isThrownBy(() -> new Order(List.of("티본스테이크-1", "티본스테이크-2", "양송이수프-1"), menuType))
                 .withMessageContaining("[ERROR]");
     }
 
@@ -22,7 +31,7 @@ public class OrderTest {
     @Test
     void should_ThrowException_When_TotalMenuCountAboveTwenty() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Order(List.of("티본스테이크-18", "초코케이크-3")))
+                .isThrownBy(() -> new Order(List.of("티본스테이크-18", "초코케이크-3"), menuType))
                 .withMessageContaining("[ERROR]");
     }
 
@@ -30,7 +39,7 @@ public class OrderTest {
     @Test
     void should_ThrowException_When_MenuCountUnderOne() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Order(List.of("티본스테이크-0")))
+                .isThrownBy(() -> new Order(List.of("티본스테이크-0"), menuType))
                 .withMessageContaining("[ERROR]");
     }
 
@@ -38,7 +47,7 @@ public class OrderTest {
     @Test
     void should_ThrowException_When_MenuTypeOnlyBeverage() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Order(List.of("제로콜라-1", "레드와인-3")))
+                .isThrownBy(() -> new Order(List.of("제로콜라-1", "레드와인-3"), menuType))
                 .withMessageContaining("[ERROR]");
     }
 
@@ -46,7 +55,7 @@ public class OrderTest {
     @Test
     void should_ThrowException_When_NotOnMenu() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Order(List.of("티본스테이크-2", "원할머니보쌈-3")))
+                .isThrownBy(() -> new Order(List.of("티본스테이크-2", "원할머니보쌈-3"), menuType))
                 .withMessageContaining("[ERROR]");
     }
 
@@ -58,7 +67,7 @@ public class OrderTest {
         @Test
         void ex1_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("-티본스테이크-2", "아이스크림-4")))
+                    .isThrownBy(() -> new Order(List.of("-티본스테이크-2", "아이스크림-4"), menuType))
                     .withMessageContaining("[ERROR]");
         }
 
@@ -66,7 +75,7 @@ public class OrderTest {
         @Test
         void ex2_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("티본스테이크-2-", "아이스크림-4")))
+                    .isThrownBy(() -> new Order(List.of("티본스테이크-2-", "아이스크림-4"), menuType))
                     .withMessageContaining("[ERROR]");
         }
 
@@ -74,7 +83,7 @@ public class OrderTest {
         @Test
         void ex3_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("티본스테이크-2-3", "아이스크림-4")))
+                    .isThrownBy(() -> new Order(List.of("티본스테이크-2-3", "아이스크림-4"), menuType))
                     .withMessageContaining("[ERROR]");
         }
 
@@ -82,7 +91,7 @@ public class OrderTest {
         @Test
         void ex4_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("티본스테이크-2- ", "아이스크림-4")))
+                    .isThrownBy(() -> new Order(List.of("티본스테이크-2- ", "아이스크림-4"), menuType))
                     .withMessageContaining("[ERROR]");
         }
 
@@ -90,7 +99,7 @@ public class OrderTest {
         @Test
         void ex5_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("티본스테이크-2", "4")))
+                    .isThrownBy(() -> new Order(List.of("티본스테이크-2", "4"), menuType))
                     .withMessageContaining("[ERROR]");
         }
 
@@ -98,7 +107,7 @@ public class OrderTest {
         @Test
         void ex6_should_ThrowException_When_FormatError() {
             assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Order(List.of("티본스테이크-2", "아이스크림")))
+                    .isThrownBy(() -> new Order(List.of("티본스테이크-2", "아이스크림"), menuType))
                     .withMessageContaining("[ERROR]");
         }
     }
@@ -106,7 +115,7 @@ public class OrderTest {
     @DisplayName("총주문 금액을 구한다.")
     @Test
     void calculateTotalOrderAmount() {
-        Order order = new Order(List.of("양송이수프-2", "티본스테이크-2", "레드와인-2", "아이스크림-2"));
+        Order order = new Order(List.of("양송이수프-2", "티본스테이크-2", "레드와인-2", "아이스크림-2"), menuType);
         int totalOrderAmount = order.calculateTotalOrderAmount();
 
         assertThat(totalOrderAmount).isEqualTo(252000);
