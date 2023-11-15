57c906c916e77338bd0aafb5a13e0d14c9fa9b23
YongHyeon <pyh4616@naver.com>
2023-11-15 14:48:35 +0900

    test(MenuTest): 주문 메뉴에 음료만 있는지 판단하는 기능 테스트

--- src/test/java/christmas/model/MenuTest.java
+++ src/test/java/christmas/model/MenuTest.java
@@ -29,4 +29,39 @@ public class MenuTest {
         assertThat(menuType.get("디저트")).isEqualTo(4);
         assertThat(menuType.get("음료")).isEqualTo(5);
     }
+
+    @DisplayName("주문에 음료만 있을 때, 메뉴 종류가 음료만 있는지 판단한다.")
+    @Test
+    void should_IsTrue_When_OrderOnlyBeverage() {
+        Map<String, Integer> menuType = new HashMap<>();
+        Menu menu = new Menu(menuType);
+        Map<String, Integer> orders = Map.of(
+                "제로콜라", 3,
+                "레드와인", 5
+        );
+        menu.countMenuType(orders);
+
+        boolean orderOnlyBeverage = menu.isOrderOnlyBeverage();
+
+        assertThat(orderOnlyBeverage).isTrue();
+    }
+
+    @DisplayName("주문에 음료만 있지 않을 때, 메뉴 종류가 음료만 있는지 판단한다.")
+    @Test
+    void should_IsFalse_When_OrderNotOnlyBeverage() {
+        Map<String, Integer> menuType = new HashMap<>();
+        Menu menu = new Menu(menuType);
+        Map<String, Integer> orders = Map.of(
+                "양송이수프", 1,
+                "타파스", 2,
+                "티본스테이크", 3,
+                "아이스크림", 4,
+                "레드와인", 5
+        );
+        menu.countMenuType(orders);
+
+        boolean orderOnlyBeverage = menu.isOrderOnlyBeverage();
+
+        assertThat(orderOnlyBeverage).isFalse();
+    }
 }
