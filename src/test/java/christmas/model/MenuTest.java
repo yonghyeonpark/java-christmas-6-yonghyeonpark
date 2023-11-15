ee09a2cfb6be836037fc96fcaedcd7eeeadb9fec
YongHyeon <pyh4616@naver.com>
2023-11-15 15:13:19 +0900

    refactor(MenuTest): 공통된 객체 생성 행위의 중복 제거, 테스트 변수를 다르게 설정

--- src/test/java/christmas/model/MenuTest.java
+++ src/test/java/christmas/model/MenuTest.java
@@ -4,16 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MenuTest {
 
+    private Map<String, Integer> menuType;
+    private Menu menu;
+
+    @BeforeEach
+    void setUp() {
+        menuType = new HashMap<>();
+        menu = new Menu(menuType);
+    }
+
     @DisplayName("주문에 대해 메뉴 종류를 분류하고 각 개수를 구한다.")
     @Test
     void countMenuType() {
-        Map<String, Integer> menuType = new HashMap<>();
-        Menu menu = new Menu(menuType);
         Map<String, Integer> orders = Map.of(
                 "양송이수프", 1,
                 "타파스", 2,
@@ -33,8 +41,6 @@ public class MenuTest {
     @DisplayName("주문에 음료만 있을 때, 메뉴 종류가 음료만 있는지 판단한다.")
     @Test
     void should_IsTrue_When_OrderOnlyBeverage() {
-        Map<String, Integer> menuType = new HashMap<>();
-        Menu menu = new Menu(menuType);
         Map<String, Integer> orders = Map.of(
                 "제로콜라", 3,
                 "레드와인", 5
@@ -49,14 +55,12 @@ public class MenuTest {
     @DisplayName("주문에 음료만 있지 않을 때, 메뉴 종류가 음료만 있는지 판단한다.")
     @Test
     void should_IsFalse_When_OrderNotOnlyBeverage() {
-        Map<String, Integer> menuType = new HashMap<>();
-        Menu menu = new Menu(menuType);
         Map<String, Integer> orders = Map.of(
-                "양송이수프", 1,
-                "타파스", 2,
-                "티본스테이크", 3,
-                "아이스크림", 4,
-                "레드와인", 5
+                "해산물파스타", 1,
+                "크리스마스파스타", 1,
+                "티본스테이크", 1,
+                "아이스크림", 2,
+                "레드와인", 2
         );
         menu.countMenuType(orders);
 
@@ -68,8 +72,6 @@ public class MenuTest {
     @DisplayName("종류로 분류한 주문의 총개수를 구한다.")
     @Test
     void calculateMenuTypeTotalCount() {
-        Map<String, Integer> menuType = new HashMap<>();
-        Menu menu = new Menu(menuType);
         Map<String, Integer> orders = Map.of(
                 "초코케이크", 1,
                 "시저샐러드", 2,
