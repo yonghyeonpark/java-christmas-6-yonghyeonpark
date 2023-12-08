ca1b8ce3b3089874ee94d7a7fbd4aeefb2a5d65e
YongHyeon <pyh4616@naver.com>
2023-12-08 16:45:07 +0900

    feat(Discount): 주말 혹은 평일 할인 금액을 계산하는 기능 구현

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -4,6 +4,8 @@ public class Discount {
 
     private static final int D_DAY_DISCOUNT_BASIC_AMOUNT = 1000;
     private static final int D_DAY_DISCOUNT_ADDITIONAL_AMOUNT = 100;
+    private static final int WEEKDAY_DISCOUNT = 2023;
+    private static final int WEEKEND_DISCOUNT = 2023;
 
     private final Date date;
     private final Order order;
@@ -18,5 +20,14 @@ public class Discount {
             return 0;
         }
         return D_DAY_DISCOUNT_BASIC_AMOUNT + ((date.getDate() - 1) * D_DAY_DISCOUNT_ADDITIONAL_AMOUNT);
+    }
+
+    public int calculateWeekEndOrWeekDayDiscount() {
+        int mainDishCount = order.countMainDish();
+        if (date.isWeekEnd()) {
+            return mainDishCount * WEEKEND_DISCOUNT;
+        }
+        int dessertCount = order.countDessert();
+        return dessertCount * WEEKDAY_DISCOUNT;
     }
 }
