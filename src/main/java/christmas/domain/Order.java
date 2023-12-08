e28cecace1c64bfd663151c7439c9852cb48c851
YongHyeon <pyh4616@naver.com>
2023-12-08 17:05:46 +0900

    feat(Order): 총 주문 금액을 구하는 기능 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import christmas.domain.menu.Appetizer;
+import christmas.domain.menu.Beverage;
 import christmas.domain.menu.Dessert;
 import christmas.domain.menu.MainDish;
 import christmas.util.Convert;
@@ -44,5 +46,56 @@ public class Order {
             }
         }
         return mainDishCount;
+    }
+
+    public int calculateTotalAmount() {
+        int totalAmount = 0;
+        for (Map.Entry<String, Integer> entry : orders.entrySet()) {
+            totalAmount += checkAppetizer(entry);
+            totalAmount += checkMainDish(entry);
+            totalAmount += checkDessert(entry);
+            totalAmount += checkBeverage(entry);
+        }
+        return totalAmount;
+    }
+
+    private int checkAppetizer(Map.Entry<String, Integer> entry) {
+        int appetizerAmount = 0;
+        for (Appetizer appetizer : Appetizer.values()) {
+            if (entry.getKey().equals(appetizer.getName())) {
+                appetizerAmount += entry.getValue() * appetizer.getPrice();
+            }
+        }
+        return appetizerAmount;
+    }
+
+    private int checkMainDish(Map.Entry<String, Integer> entry) {
+        int mainDishAmount = 0;
+        for (MainDish mainDish : MainDish.values()) {
+            if (entry.getKey().equals(mainDish.getName())) {
+                mainDishAmount += entry.getValue() * mainDish.getPrice();
+            }
+        }
+        return mainDishAmount;
+    }
+
+    private int checkDessert(Map.Entry<String, Integer> entry) {
+        int dessertAmount = 0;
+        for (Dessert dessert : Dessert.values()) {
+            if (entry.getKey().equals(dessert.getName())) {
+                dessertAmount += entry.getValue() * dessert.getPrice();
+            }
+        }
+        return dessertAmount;
+    }
+
+    private int checkBeverage(Map.Entry<String, Integer> entry) {
+        int beverageAmount = 0;
+        for (Beverage beverage : Beverage.values()) {
+            if (entry.getKey().equals(beverage.getName())) {
+                beverageAmount += entry.getValue() * beverage.getPrice();
+            }
+        }
+        return beverageAmount;
     }
 }
