507228ad4b7003d6c5030811e58fe81111a7f9cf
YongHyeon <pyh4616@naver.com>
2023-12-08 16:49:47 +0900

    refactor(Discount): 명확한 표현을 위해 메서드와 변수 이름 뒤에 Amount를 추가

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -4,9 +4,9 @@ public class Discount {
 
     private static final int D_DAY_DISCOUNT_BASIC_AMOUNT = 1000;
     private static final int D_DAY_DISCOUNT_ADDITIONAL_AMOUNT = 100;
-    private static final int WEEKDAY_DISCOUNT = 2023;
-    private static final int WEEKEND_DISCOUNT = 2023;
-    private static final int SPECIAL_DAY_DISCOUNT = 1000;
+    private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
+    private static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
+    private static final int SPECIAL_DAY_DISCOUNT_AMOUNT = 1000;
 
     private final Date date;
     private final Order order;
@@ -16,25 +16,25 @@ public class Discount {
         this.order = order;
     }
 
-    public int calculateDDayDiscount() {
+    public int calculateDDayDiscountAmount() {
         if (date.isOverChristmas()) {
             return 0;
         }
         return D_DAY_DISCOUNT_BASIC_AMOUNT + ((date.getDate() - 1) * D_DAY_DISCOUNT_ADDITIONAL_AMOUNT);
     }
 
-    public int calculateWeekEndOrWeekDayDiscount() {
+    public int calculateWeekEndOrWeekDayDiscountAmount() {
         int mainDishCount = order.countMainDish();
         if (date.isWeekEnd()) {
-            return mainDishCount * WEEKEND_DISCOUNT;
+            return mainDishCount * WEEKEND_DISCOUNT_AMOUNT;
         }
         int dessertCount = order.countDessert();
-        return dessertCount * WEEKDAY_DISCOUNT;
+        return dessertCount * WEEKDAY_DISCOUNT_AMOUNT;
     }
 
-    public int calculateSpecialDayDiscount() {
+    public int calculateSpecialDayDiscountAmount() {
         if (date.isSpecialDay()) {
-            return SPECIAL_DAY_DISCOUNT;
+            return SPECIAL_DAY_DISCOUNT_AMOUNT;
         }
         return 0;
     }
