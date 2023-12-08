525419121def03b58623d948753ecfa2546f74b0
YongHyeon <pyh4616@naver.com>
2023-12-08 16:14:54 +0900

    feat(Appetizer): 에피타이저를 관리하는 Enum 구현

---
+++ src/main/java/christmas/domain/menu/Appetizer.java
@@ -0,0 +1,24 @@
+package christmas.domain.menu;
+
+public enum Appetizer {
+
+    MUSHROOM_SOUP("양송이수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000);
+
+    private String name;
+    private int price;
+
+    Appetizer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
