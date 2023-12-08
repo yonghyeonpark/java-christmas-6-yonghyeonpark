80b9a9a12dd7a4ee637c3f592616a9fd0ab57443
YongHyeon <pyh4616@naver.com>
2023-12-08 22:42:35 +0900

    refactor(Convert): 문자열의 리스트 변환에서 잘못된 형식에 대한 유효성 검사 기능 추가

--- src/main/java/christmas/util/Convert.java
+++ src/main/java/christmas/util/Convert.java
@@ -13,11 +13,25 @@ public class Convert {
         }
     }
 
-    public static List<String> stringToListByComma(String input) {
+    public static List<String> stringToListByComma(String input, String errorMessage) {
+        validateEndComma(input, errorMessage);
         return Arrays.stream(input.split(",")).toList();
     }
 
-    public static List<String> stringToListByDash(String input) {
+    private static void validateEndComma(String input, String errorMessage) {
+        if (input.endsWith(",")) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public static List<String> stringToListByDash(String input, String errorMessage) {
+        validateEndDash(input, errorMessage);
         return Arrays.stream(input.split("-")).toList();
+    }
+
+    private static void validateEndDash(String input, String errorMessage) {
+        if (input.endsWith("-")) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
