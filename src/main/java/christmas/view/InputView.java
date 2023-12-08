77341d1c71c3b456bb6606c3a039d5e351dc6d06
YongHyeon <pyh4616@naver.com>
2023-12-08 15:17:08 +0900

    feat(InputView): 날짜와 주문을 입력하는 기능 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String readDate() {
+        return readLine();
+    }
+
+    public String readOrder() {
+        return readLine();
+    }
+}
