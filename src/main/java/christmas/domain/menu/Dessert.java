64aac52df9500eab8a7fff2233cbc5d84d7fb1f4
YongHyeon <pyh4616@naver.com>
2023-12-08 16:16:35 +0900

    feat(Dessert): 디저트를 관리하는 Enum 구현

---
+++ src/main/java/christmas/domain/menu/Dessert.java
@@ -0,0 +1,22 @@
+package christmas.domain.menu;
+
+public enum Dessert {
+    CHOCOLATE_CAKE("초코케이크", 15_000),
+    ICE_CREAM("아이스크림", 5_000);
+
+    private String name;
+    private int price;
+
+    Dessert(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
