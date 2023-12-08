db2cf619cde39f15df4fdb6194bb639031b09481
YongHyeon <pyh4616@naver.com>
2023-12-08 22:44:19 +0900

    refactor(Order): 주문에 대한 유효성 검사 기능 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -11,6 +11,8 @@ import java.util.Map;
 
 public class Order {
 
+    private static final int MIN_ORDER_COUNT = 1;
+    private static final int MAX_ORDERS_COUNT = 20;
     private static final String ORDER_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private static final String BLANK = " ";
     private static final String UNIT = "개";
@@ -21,13 +23,109 @@ public class Order {
     public Order(String inputOrders) {
         this.orders = new HashMap<>();
         setUp(inputOrders);
+        validateCount();
+        validateOnlyBeverage();
     }
 
     private void setUp(String inputOrders) {
-        List<String> orders = Convert.stringToListByComma(inputOrders);
+        List<String> orders = Convert.stringToListByComma(inputOrders, ORDER_ERROR_MESSAGE);
         for (String order : orders) {
-            List<String> menuAndCount = Convert.stringToListByDash(order);
+            List<String> menuAndCount = Convert.stringToListByDash(order, ORDER_ERROR_MESSAGE);
+            validateFormat(menuAndCount);
             this.orders.put(menuAndCount.get(0), Convert.stringToInt(menuAndCount.get(1), ORDER_ERROR_MESSAGE));
+        }
+        validateDuplicate(orders);
+        validateMenu(orders);
+    }
+
+    private void validateFormat(List<String> menuAndCount) {
+        if (menuAndCount.size() != 2) {
+            throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<String> orders) {
+        if (this.orders.size() != orders.size()) {
+            throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateMenu(List<String> orders) {
+        int menuTypeCount = 0;
+        for (Map.Entry<String, Integer> entry : this.orders.entrySet()) {
+            menuTypeCount += getAppetizerCount(entry);
+            menuTypeCount += getMainDishCount(entry);
+            menuTypeCount += getDessertCount(entry);
+            menuTypeCount += getBeverageCount(entry);
+        }
+        if (orders.size() != menuTypeCount) {
+            throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
+        }
+    }
+
+    private int getAppetizerCount(Map.Entry<String, Integer> entry) {
+        int appetizerCount = 0;
+        for (Appetizer appetizer : Appetizer.values()) {
+            if (entry.getKey().contains(appetizer.getName())) {
+                appetizerCount++;
+            }
+        }
+        return appetizerCount;
+    }
+
+    private int getMainDishCount(Map.Entry<String, Integer> entry) {
+        int mainDishCount = 0;
+        for (MainDish mainDish : MainDish.values()) {
+            if (entry.getKey().contains(mainDish.getName())) {
+                mainDishCount++;
+            }
+        }
+        return mainDishCount;
+    }
+
+    private int getDessertCount(Map.Entry<String, Integer> entry) {
+        int DessertCount = 0;
+        for (Dessert dessert : Dessert.values()) {
+            if (entry.getKey().contains(dessert.getName())) {
+                DessertCount++;
+            }
+        }
+        return DessertCount;
+    }
+
+    private int getBeverageCount(Map.Entry<String, Integer> entry) {
+        int beverageCount = 0;
+        for (Beverage beverage : Beverage.values()) {
+            if (entry.getKey().contains(beverage.getName())) {
+                beverageCount++;
+            }
+        }
+        return beverageCount;
+    }
+
+
+    private void validateCount() {
+        int totalCount = 0;
+        for (int count : orders.values()) {
+            if (count < MIN_ORDER_COUNT) {
+                throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
+            }
+            totalCount += count;
+        }
+        if (totalCount > MAX_ORDERS_COUNT) {
+            throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateOnlyBeverage() {
+        int beverageCount = 0;
+        for (Beverage beverage : Beverage.values()) {
+            if (orders.containsKey(beverage)) {
+                beverageCount++;
+            }
+        }
+        if (beverageCount == orders.size()) {
+            throw new IllegalArgumentException(ORDER_ERROR_MESSAGE);
         }
     }
 
