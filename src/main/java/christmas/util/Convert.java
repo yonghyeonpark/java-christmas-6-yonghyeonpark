03d1406c8929c5c2b0bd8412b74f2c727a43aa18
YongHyeon <pyh4616@naver.com>
2023-12-08 15:43:10 +0900

    feat(Convert): 문자열을 콤마를 기준으로, 대쉬를 기준으로, 리스트로 변환하는 기능 구현

--- src/main/java/christmas/util/Convert.java
+++ src/main/java/christmas/util/Convert.java
@@ -1,5 +1,8 @@
 package christmas.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Convert {
 
     public static int stringToInt(String input, String errorMessage) {
@@ -8,5 +11,13 @@ public class Convert {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(errorMessage);
         }
+    }
+
+    public static List<String> stringToListByComma(String input) {
+        return Arrays.stream(input.split(",")).toList();
+    }
+
+    public static List<String> stringToListByDash(String input) {
+        return Arrays.stream(input.split("-")).toList();
     }
 }
