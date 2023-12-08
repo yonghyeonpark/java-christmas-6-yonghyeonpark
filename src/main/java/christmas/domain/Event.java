5697cbf90564c46559ed0efa5285e3c2907ea2d1
YongHyeon <pyh4616@naver.com>
2023-12-08 17:34:37 +0900

    feat(Event): 혜택 금액에 따라 뱃지를 얻는 기능 구현

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -3,6 +3,7 @@ package christmas.domain;
 public class Event {
 
     private static final int EVENT_APPLICATION_CRITERIA = 10000;
+    private static final String NOTHING = "없음";
 
     private final Order order;
     private final Benefit benefit;
@@ -17,5 +18,19 @@ public class Event {
             return true;
         }
         return false;
+    }
+
+    public String getBadge() {
+        int totalBenefitAmount = benefit.calculateTotalAmount();
+        if (totalBenefitAmount >= Badge.STAR.getStandard() && totalBenefitAmount < Badge.TREE.getStandard()) {
+            return Badge.STAR.getName();
+        }
+        if (totalBenefitAmount >= Badge.TREE.getStandard() && totalBenefitAmount < Badge.SANTA.getStandard()) {
+            return Badge.TREE.getName();
+        }
+        if (totalBenefitAmount >= Badge.SANTA.getStandard()) {
+            return Badge.SANTA.getName();
+        }
+        return NOTHING;
     }
 }
