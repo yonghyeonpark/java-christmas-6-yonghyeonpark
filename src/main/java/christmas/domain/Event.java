de9a5c3b47f88858f0cf8d2386fb8cefee31e981
YongHyeon <pyh4616@naver.com>
2023-12-08 17:38:00 +0900

    feat(Event): 구매 금액에 따라 증정품을 얻는 기능 구현

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -3,7 +3,9 @@ package christmas.domain;
 public class Event {
 
     private static final int EVENT_APPLICATION_CRITERIA = 10000;
+    private static final int GIFT_APPLICATION_CRITERIA = 120000;
     private static final String NOTHING = "없음";
+    private static final String GIFT = "샴페인 1개";
 
     private final Order order;
     private final Benefit benefit;
@@ -30,6 +32,13 @@ public class Event {
         }
         if (totalBenefitAmount >= Badge.SANTA.getStandard()) {
             return Badge.SANTA.getName();
+        }
+        return NOTHING;
+    }
+
+    public String getGift() {
+        if (order.calculateTotalAmount() > GIFT_APPLICATION_CRITERIA) {
+            return GIFT;
         }
         return NOTHING;
     }
